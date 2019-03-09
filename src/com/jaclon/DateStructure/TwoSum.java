ff3735002a872265f4342615bd47c79073ee5e73
jaclon-m <jaclon_m@163.com>
2019-03-09 17:16:05 +0800

    two sum

---
+++ src/com/jaclon/DateStructure/TwoSum.java
@@ -0,0 +1,31 @@
+package com.jaclon.DateStructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//两数加和
+public class TwoSum {
+    //暴力法O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0;i<nums.length-1;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+    //哈希表，O(n),空间复杂度O(n)
+    public int[] twoSum2(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap <>();
+        for(int i = 0;i<nums.length;i++){
+            if(map.get(target-nums[i])!= null){
+                return new int[]{i,map.get(target-nums[i])};
+            }else {
+                map.put(nums[i],i);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
