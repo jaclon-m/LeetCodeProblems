b41682a973c45bd01e796d9de90b97d02c4d75af
jaclon-m <jaclon_m@163.com>
2019-08-29 10:43:42 +0800

    sqrt

--- src/com/jaclon/dateStructure/search/BinarySearch.java
+++ src/com/jaclon/dateStructure/search/BinarySearch.java
@@ -11,6 +11,12 @@ public class BinarySearch {
 
     }
 
+    /**
+     * 非递归实现
+     * @param array
+     * @param des
+     * @return
+     */
     public static int search(int[] array , int des){
         int low = 0;
         int high = array.length - 1;
@@ -28,4 +34,32 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 递归实现
+     * @param array
+     * @param n
+     * @param value
+     * @return
+     */
+    public static int bsearch(int[] array , int n , int value){
+        return bsearchInternally(array,0,n-1,value);
+    }
+
+    private static int bsearchInternally(int[] array, int low, int high, int value) {
+
+        if(low > high){
+            return -1;
+        }
+
+        int mid = low + (high - low)>>1;
+        if(array[mid] == value){
+            return mid;
+        }else if(array[mid] > value){
+            return bsearchInternally(array,low,mid - 1 ,value);
+        }else {
+            return bsearchInternally(array,mid + 1,high,value);
+        }
+    }
+
 }
---
+++ src/com/jaclon/dateStructure/search/Sqrt_69.java
@@ -0,0 +1,41 @@
+package com.jaclon.dateStructure.search;
+
+/**
+ * @author jaclon
+ * @date 2019/8/29
+ */
+public class Sqrt_69 {
+
+    public int mySqrt(int x) {
+        //还有bug,暂时未完成
+        int low = 1 ,high = x;
+        //防止溢出
+        while (low <= high){
+            if(high - low == 1){
+                return low;
+            }
+            int mid = low + ((high - low)>>1);
+            if(x/mid > mid){
+
+                low = mid + 1;
+            }else if(x / mid < mid){
+
+                high = mid - 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int mySqrt2(int x){
+        if( x == 0 || x == 1){
+            return x;
+        }
+        int a = x >> 1;
+        while (a> x/a){
+            a = (a + x/a)/2;
+        }
+        return a;
+    }
+}
