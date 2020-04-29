f7f9fec4d06a9cf7560f8133aceb5bc356d418fa
jaclon-m <jaclon_m@163.com>
2020-04-29 08:36:15 +0800

    1

--- src/com/jaclon/datestructure/addmeet/ReverseArray03.java
+++ src/com/jaclon/datestructure/addmeet/ReverseArray03.java
@@ -1,9 +1,19 @@
 package com.jaclon.datestructure.addmeet;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @author jaclon
  * @date 2020/4/23
  * TODO 旋转数组
  */
 public class ReverseArray03 {
+
+    public static void main(String[] args) {
+        Map<String,String> map = new LinkedHashMap<>();
+        map.put("1","s");
+        int[] a = new int[10];
+        System.out.println(a[2]);
+    }
 }
