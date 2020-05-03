4c02a1e66e59e6f1950558f451f7bf28169cb557
jaclon-m <jaclon_m@163.com>
2020-05-03 12:36:44 +0800

    层序遍历

--- src/com/jaclon/datestructure/binarytree/LevelOrderTraOfBT_102.java
+++ src/com/jaclon/datestructure/binarytree/LevelOrderTraOfBT_102.java
@@ -4,12 +4,43 @@ import com.jaclon.datestructure.basic.TreeNode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author jaclon
  * @date 2019/9/28
  */
 public class LevelOrderTraOfBT_102 {
+    /**
+     * 比较原答案的优势是使用了queue。
+     * 注意：while(size -- > 0) 这里的思路是把一层的先遍历完。自己做的时候没加这个条件会导致先遍历左子树的下层。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> result = new LinkedList <>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList <>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new LinkedList <>();
+            while (size -- > 0){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         LinkedList <List <Integer>> lists = new LinkedList <>();
@@ -44,4 +75,6 @@ public class LevelOrderTraOfBT_102 {
         return lists;
 
     }
+
+
 }
--- src/com/jaclon/datestructure/geekbandcard/Solution.java
+++ src/com/jaclon/datestructure/geekbandcard/Solution.java
@@ -1,9 +1,9 @@
 package com.jaclon.datestructure.geekbandcard;
 
 import com.jaclon.datestructure.basic.ListNode;
+import com.jaclon.datestructure.basic.TreeNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
