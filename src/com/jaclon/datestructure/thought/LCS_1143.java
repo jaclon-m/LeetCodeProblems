0a0b80fc40bf5a14a7a23586987f5b748803d50d
jaclon-m <jaclon_m@163.com>
2020-05-20 10:02:13 +0800

    字符匹配整理

--- src/com/jaclon/datestructure/thought/LCS_1143.java
+++ src/com/jaclon/datestructure/thought/LCS_1143.java
@@ -2,6 +2,7 @@ package com.jaclon.datestructure.thought;
 
 /**
  * 最长公共子序列
+ * https://www.cnblogs.com/chenleideblog/p/10455723.html
  * @author jaclon
  * @date 2020/5/19
  */
@@ -25,5 +26,42 @@ public class LCS_1143 {
             }
         }
         return maxC[n][m];
+    }
+    //扩展 求匹配字符
+    private char[] a = "mitcmu".toCharArray();
+    private char[] b = "mtacnu".toCharArray();
+    private int n = 6;
+    private int m = 6;
+    int[][] maxLcs = new int[n+1][m+1];
+    int[][] dist = new int[n+1][m+1];
+    private void lscDPWithDist() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i - 1] == b[j - 1]) {
+                    maxLcs[i][j] = maxLcs[i - 1][j - 1] + 1;
+                    dist[i][j] = 1;
+                } else {
+                    if (maxLcs[i][j - 1] > maxLcs[i - 1][j]) {
+                        maxLcs[i][j] = maxLcs[i][j - 1];
+                        dist[i][j] = 2;
+                    } else {
+                        maxLcs[i][j] = maxLcs[i - 1][j];
+                        dist[i][j] = 3;
+                    }
+                }
+            }
+        }
+    }
+
+    private void print(int i,int j){
+        if(i == 0|| j == 0) return;
+        if(dist[i][j] ==1){
+            print(i-1,j-1);
+            System.out.print(a[i-1]+ "\t");
+        }else if(dist[i][j] == 2){
+            print(i,j-1);
+        }else {
+            print(i-1,j);
+        }
     }
 }
