290548b7c28acafcaec21308b84f38719375fa3c
jaclon-m <jaclon_m@163.com>
2020-06-30 10:42:52 +0800

    l

--- src/com/jaclon/datestructure/Solution.java
+++ src/com/jaclon/datestructure/Solution.java
@@ -1,13 +1,9 @@
 package com.jaclon.datestructure;
 
-import com.jaclon.datestructure.basic.ListNode;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
+import java.util.Scanner;
 
 /**
  * n 皇后问题
@@ -16,22 +12,32 @@ import java.util.logging.Level;
  * @date 2020/4/20
  */
 public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        if(s.isEmpty()){
-            return 0;
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int n =scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = scanner.nextLine();
+            list.add(s);
         }
-        char[] array = s.toCharArray();
-        int a = 0,b = 0;
-        int maxl = 0;
-        while (b+1 <array.length){
-            if(array[b]!= array[b+1]){
-                int value = b-a+2;
-                maxl = maxl > value?maxl:value;
-            }else {
-                a = b+1;
-            }
-            b = b+1;
+        System.out.println(list.size());
+        for(String s: list){
+            System.out.println(validate(s));
         }
-        return  maxl;
     }
+
+    private static String validate(String s) {
+        int n = s.length();
+        for (int i = 0; i < n - 2; i++) {
+            if(s.charAt(i) == s.charAt(i+1) && s.charAt(i) == s.charAt(i+2)){
+                s.replaceFirst(s.charAt(i) + "","");
+            }
+            if(i < n-3 && s.charAt(i) == s.charAt(i+1) && s.charAt(i+2)==s.charAt(i+3)){
+                s.replaceFirst(s.charAt(i+2)+ "","");
+            }
+        }
+        return s;
+    }
+
+
 }
